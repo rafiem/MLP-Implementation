9d76c76e4b114a1b715b5c95f4e94c5b2a6e760b
rafiem <rafiemuhammad07@gmail.com>
2019-03-17 01:59:49 +0700

    Change init variable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mlp import MLP
 filename      = "iris.csv"
 test_size     = 50
 learning_rate = 0.1
-seed          = 5
+seed          = 1337
 epoch         = 275
 theta_input   = [[0.4, 0.6, 0.2, 0.55], [0.6, 0.3, 0.45, 0.55], [0.35, 0.75, 0.45, 0.4], [0.55, 0.8, 0.25, 0.2]]
 theta_hidden  = [[0.4, 0.25, 0.7, 0.35], [0.65, 0.40, 0.3, 0.7]]
